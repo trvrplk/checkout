c272e673093f80a4a8568721e6f55d726d8bdc8f
T J <myface13214@T-Js-MacBook.local>
2011-12-13 07:56:45 -0500

    fixed stuff

--- checkout.gemspec
+++ checkout.gemspec
@@ -20,6 +20,6 @@ Gem::Specification.new do |s|
 
   # specify any dependencies here; for example:
   s.add_development_dependency "rspec"
-  s.add_dependency "parslet"
+  s.add_dependency "treetop" 
   # s.add_runtime_dependency "rest-client"
 end
--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,11 +1,11 @@
 require "checkout/version"
-require 'parslet'
+require 'treetop'
 
-module Checkout
-  def self.parse_md(content)
-	  cont = "#{content}".gsub(/^# $/){nil}
-	  "#{cont}".gsub(/^# (.+)$/){"<h1>#{cont}</h1>"}
-	  cont = "#{cont}".gsub(/^__$/){nil}
-	  "#{cont}".gsub(/^__(.+)__$/){"<strong>#{cont}</strong>"}
+module Checkout	
+	def self.parse_md(content) 
+		cont = "#{content}".gsub(/^# $/){nil}
+		"#{cont}".gsub(/^# (.+)$/){"<h1>#{cont}</h1>"}
+		cont = "#{cont}".gsub(/^__$/){nil}
+		"#{cont}".gsub(/^__(.+)__$/){"<strong>#{cont}</strong>"}	
 	end  
 end
